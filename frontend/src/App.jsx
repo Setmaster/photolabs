de6e31ebc01c35853a69f3b26c2233928a27827c
Victor <vi7org@hotmail.com>
2024-11-28 14:13:53 -0500

    display an array of photo items

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -14,17 +14,26 @@ const sampleDataForPhotoListItem = {
     profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
 };
 
+const photos = [sampleDataForPhotoListItem, sampleDataForPhotoListItem, sampleDataForPhotoListItem];
+
 // Note: Rendering a single component to build components in isolation
 const App = () => {
+    const photoItems = photos.map((photo)=>{
+        return (
+            <PhotoListItem
+                key={sampleDataForPhotoListItem.id}
+                id={sampleDataForPhotoListItem.id}
+                location={sampleDataForPhotoListItem.location}
+                imageSource={sampleDataForPhotoListItem.imageSource}
+                username={sampleDataForPhotoListItem.username}
+                profile={sampleDataForPhotoListItem.profile}
+            />
+        );
+    })
+    
   return (
     <div className="App">
-      <PhotoListItem
-          id={sampleDataForPhotoListItem.id}
-          location={sampleDataForPhotoListItem.location}
-          imageSource={sampleDataForPhotoListItem.imageSource}
-          username={sampleDataForPhotoListItem.username}
-          profile={sampleDataForPhotoListItem.profile}
-      />
+        {photoItems}
     </div>
   );
 };
