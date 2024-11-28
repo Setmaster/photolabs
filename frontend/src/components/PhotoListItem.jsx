47ca92bb32df649b38281c8f3c357cf42ce1d0b7
Victor <vi7org@hotmail.com>
2024-11-28 14:09:24 -0500

    fix PhotoListItem.jsx styling

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-
 import "../styles/PhotoListItem.scss";
 
 const PhotoListItem = ({
@@ -11,12 +10,12 @@ const PhotoListItem = ({
                        }) => {
     return (
         <div className="photo-list__item">
-            <img className="photo-list-item__image" src={imageSource} alt={`Photo by ${username}`}/>
-            <div className="photo-list-item__info">
-                <img className="photo-list-item__profile" src={profile} alt={`${username}'s profile`}/>
-                <div className="photo-list-item__details">
-                    <h3>{username}</h3>
-                    <p>{city}, {country}</p>
+            <img className="photo-list__image" src={imageSource} alt={`Photo by ${username}`} />
+            <div className="photo-list__user-info">
+                <img className="photo-list__user-profile" src={profile} alt={`${username}'s profile`} />
+                <div className="photo-list__user-details">
+                    <h3 className="photo-list__user-username">{username}</h3>
+                    <p className="photo-list__user-location">{city}, {country}</p>
                 </div>
             </div>
         </div>
