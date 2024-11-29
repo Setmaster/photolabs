911f8e42085957c2812e45514eeac4b758227009
Victor <vi7org@hotmail.com>
2024-11-29 11:26:34 -0500

    implement TopNavigationBar.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 import './App.scss';
 import PhotoList from "./components/PhotoList";
-import TopicList from "./components/TopicList";
+import TopNavigationBar from "./components/TopNavigationBar";
 
 // Note: Rendering a single component to build components in isolation
 const App = () => {
@@ -10,7 +10,7 @@ const App = () => {
     
   return (
     <div className="App">
-        <TopicList/>
+        <TopNavigationBar/>
         <PhotoList/>
     </div>
   );
