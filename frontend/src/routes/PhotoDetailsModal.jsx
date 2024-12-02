db8327bcf6e5f2dbf11ab20a538c9b72b23adf68
Victor <vi7org@hotmail.com>
2024-12-02 02:19:19 -0500

    implement close button for modal, show pointer cursor on photo hover

--- frontend/src/routes/PhotoDetailsModal.jsx
+++ frontend/src/routes/PhotoDetailsModal.jsx
@@ -1,13 +1,20 @@
-import React from 'react';
+import React, {useContext} from 'react';
 
 import '../styles/PhotoDetailsModal.scss'
 import closeSymbol from '../assets/closeSymbol.svg';
+import {ModalContext} from "../context/ModalContext";
 
 const PhotoDetailsModal = () => {
+    const {isModalOpen, toggleModal} = useContext(ModalContext);
+    
   return (
     <div className="photo-details-modal">
       <button className="photo-details-modal__close-button">
-        <img src={closeSymbol} alt="close symbol" />
+        <img
+            src={closeSymbol}
+            alt="close symbol"
+            onClick={toggleModal}
+        />
       </button>
     </div>
   )
